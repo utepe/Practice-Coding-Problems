2c859953d70982fcc26bbe20936797eccd6e67d9
Uygur Tepe <uygur.t10@gmail.com>
2020-01-02 13:47:20 -0500

    added DCP#69

---
+++ Daily Coding Problems/DCP#69.py
@@ -0,0 +1,14 @@
+def maxProduct(list, n):
+    if len(list) < 3:
+        return "Not enough values in list"
+    elif len(list) == 3:
+        return list[0] * list[1] * list[2]
+    else:
+        list.sort()
+        return max(list[0]*list[1]*list[n-1], list[n-3]*list[n-2]*list[n-1])
+
+if __name__ == '__main__':
+    list = [-10, -10, 5, 2]
+    n = len(list)
+    maxProd = maxProduct(list, n)
+    print(maxProd)
